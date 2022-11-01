461588c582d7d939516d470b47229f47e211d8ff
srivathsan861 <srivathsan861@gmail.com>
2022-11-01 23:59:25 +0530

    partial approach for wave sort

---
+++ Code/15)More_Sorting_Techniques/04)wave_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+/*
+QUESTION: Sort the array in such a way that ith inex is greater then the (i+1)th index and (i+2)th index should 
+          be lesser then (i+1)th index at every instance.
+EXAMPLE: 1,3,4,2,7,5
+ ->BRUTE-FORCE:
+   *first sort the array -> 1,2,3,4,5,7
+   *couple swap -> 2,1,4,3,7,5
+   *TIME COMPLEXITY: O(N LOG N)
+ 
+NOTE: 
+*Most asked interview problem
+*/
+
+int main(){
+
+return 0;
+}
